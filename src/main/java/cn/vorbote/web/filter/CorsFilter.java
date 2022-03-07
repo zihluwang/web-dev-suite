5b00600d6ff6763bb13484edf6564e7eaa08b3c0
vorbote <theodore0126@outlook.com>
2022-03-07 08:24:36 +0800

    fix: solved the problem that spring wont prompt the configuration

--- src/main/java/cn/vorbote/web/filter/CorsFilter.java
+++ src/main/java/cn/vorbote/web/filter/CorsFilter.java
@@ -44,7 +44,7 @@ public class CorsFilter implements Filter {
     /**
      * According to <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Expose-Headers"
      * >MDN Docs</a>, this response header allows a server to indicate which response headers should be made available
-     * to scripts running in the browser, in response to a cross-origin request.<br/>
+     * to scripts running in the browser, in response to a cross-origin request.<br>
      * Only the <a href="https://developer.mozilla.org/en-US/docs/Glossary/CORS-safelisted_response_header"
      * >CORS-safelisted</a> response headers are exposed by default. For clients to be able to access other headers,
      * the server must list them using the Access-Control-Expose-Headers header.
